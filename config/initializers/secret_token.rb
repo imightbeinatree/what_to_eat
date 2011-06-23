1c2234c39891e7d2a80a22128b4c1b2469c92433
Michael Orr <michael@cloudspace.com>
2011-06-23 14:48:19 -0400

    initial generation of project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatToEat::Application.config.secret_token = '24365875bb7aaef82dc677c29f7352718fc4ce0f26d32d6398f6625818b6f0864b62082f4d1e031cb91dba5d79776246384e40827bced879168443cda45738dd'
