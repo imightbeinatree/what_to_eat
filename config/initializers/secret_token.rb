e62f8b316ecbac8c112934ee2a6b90d867a05dbe
Michael Orr <michael@cloudspace.com>
2011-06-23 14:51:23 -0400

    using mysql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-WhatToEat::Application.config.secret_token = '24365875bb7aaef82dc677c29f7352718fc4ce0f26d32d6398f6625818b6f0864b62082f4d1e031cb91dba5d79776246384e40827bced879168443cda45738dd'
+WhatToEat::Application.config.secret_token = 'e4b94125058296391d68122a840be7f15ade3a2586ac5d9daee23fcb3ad86c8505c16af016d1cbdd1966b3b24b121fd90102bd1ca775d79ec8b23f09cd895386'
